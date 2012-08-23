7538edc161368882c4d817174b97678fb2eb1edf
bray <brian.ray1@gmail.com>
2012-08-23 14:19:36 -0400

    Getting cap setup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,7 @@
+graphiti_root = File.join(File.dirname(__FILE__), '..')
+
 listen 5001 # by default Unicorn listens on port 8080
 worker_processes 2 # this should be >= nr_cpus
-pid "/var/sites/graphiti/shared/pids/unicorn.pid"
-stderr_path "/var/sites/graphiti/shared/log/unicorn.log"
-stdout_path "/var/sites/graphiti/shared/log/unicorn.log"
+pid "#{graphiti_root}/tmp/pids/unicorn.pid"
+stderr_path "#{graphiti_root}/log/unicorn.log"
+stdout_path "#{graphiti_root}/log/unicorn.log"
